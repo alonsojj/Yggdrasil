7ea4fdc7ecb8ee5f52da3b6649752c0f6cfda8af
alonsojj <juan13032007@gmail.com>
2026-01-20 01:26:11 -0300

    feat: implement addon instalation,deletation and update

--- app/services/realms_engine/installer.py
+++ app/services/realms_engine/installer.py
@@ -1,4 +1,51 @@
+import asyncio
+import os
+from pathlib import Path
+import shutil
+import stat
+
+
 class RealmsInstaller:
-    async def install_from_git(self, url: str) -> str:
-        # TODO: Implement git clone and installation logic
-        pass
+    def delete_realm(self, folder_path: Path):
+        folder_path = str(folder_path)
+
+        if not os.path.exists(folder_path):
+            return True
+
+        def remove_readonly(func, path, excinfo):
+            os.chmod(path, stat.S_IWRITE)
+            func(path)
+
+        try:
+            shutil.rmtree(folder_path, onerror=remove_readonly)
+            return True
+        except Exception:
+            return False
+
+    async def update_from_git(self, folder_path: Path):
+        command = "git"
+        args = ["-C", folder_path, "pull"]
+
+        process = await asyncio.create_subprocess_exec(
+            command,
+            *args,
+        )
+        await process.communicate()
+        if process.returncode == 0:
+            return True
+        else:
+            return False
+
+    async def install_from_git(self, url: str, folder_name: Path) -> bool:
+        command = "git"
+        args = ["clone", url, folder_name]
+
+        process = await asyncio.create_subprocess_exec(
+            command,
+            *args,
+        )
+        await process.communicate()
+        if process.returncode == 0:
+            return True
+        else:
+            return False
